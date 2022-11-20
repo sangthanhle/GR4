e8ff8e7decb229f82c68ba657896cccdba5ccbb0
sangthanhle <118423537+sangthanhle@users.noreply.github.com>
2022-11-20 18:52:34 +0700

    Add files via upload

---
+++ CHUVI_DIENTICH_HCN.sql
@@ -0,0 +1,9 @@
+﻿--Chương trình tính diện tích, chu vi hình chữ nhật khi biết chiều dài và chiều rộng.
+DECLARE @Chieu_Dai int , @Chieu_Rong int ,@Dien_Tich int , @Chu_Vi int
+SET @Chieu_Dai =5;
+SET @Chieu_Rong = 5;
+SET @Chu_Vi = (@Chieu_Rong + @Chieu_Dai)*2
+SET @Dien_Tich = @Chieu_Rong * @Chieu_Dai
+SELECT @Chu_Vi AS N'CHU VI HINH CHU NHAT'
+SELECT @Dien_Tich AS N'DIEN TICH HINH CHU NHAT'
+
