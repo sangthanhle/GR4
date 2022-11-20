7a70aadc6e70f2d09cc4c2322ef988a0fb827569
sangthanhle <118423537+sangthanhle@users.noreply.github.com>
2022-11-20 18:55:22 +0700

    Update SQLQuery4Lab2.sql

--- SQLQuery4Lab2.sql
+++ SQLQuery4Lab2.sql
@@ -1,4 +1,5 @@
-﻿SELECT TENPHG AS N'TÊN PHÒNG',COUNT(*) AS N'SỐ LƯỢNG ĐỀ ÁN'
+--Với mỗi phòng ban, cho biết tên phòng ban và số lượng đề án mà phòng ban đó chủ trì
+SELECT TENPHG AS N'TÊN PHÒNG',COUNT(*) AS N'SỐ LƯỢNG ĐỀ ÁN'
 FROM DEAN,PHONGBAN
 WHERE DEAN.PHONG=PHONGBAN.MAPHG
 GROUP BY TENPHG
