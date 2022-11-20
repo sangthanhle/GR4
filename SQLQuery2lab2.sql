0977cc0cf22a0abb2bf02aaa0a228464b05b68d1
sangthanhle <118423537+sangthanhle@users.noreply.github.com>
2022-11-20 18:53:48 +0700

    Update SQLQuery2lab2.sql

--- SQLQuery2lab2.sql
+++ SQLQuery2lab2.sql
@@ -1,4 +1,6 @@
-﻿DECLARE @Tb_Luong FLOAT
+--Cho biết họ tên nhân viên (HONV, TENLOT, TENNV) có mức lương
+trên mức lương trung bình của phòng "Nghiên cứu”
+DECLARE @Tb_Luong FLOAT
 SET @Tb_Luong = (SELECT AVG(LUONG) FROM NHANVIEN)
 SELECT HONV + TENLOT + TENNV as 'Ho va Ten' , LUONG ,TENPHG as 'phong ban'
 FROM NHANVIEN , PHONGBAN 
