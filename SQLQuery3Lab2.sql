6dfd9d82a4c0c076f5ac5a34fad7e8ae6867e6e3
sangthanhle <118423537+sangthanhle@users.noreply.github.com>
2022-11-20 18:54:33 +0700

    Update SQLQuery3Lab2.sql

--- SQLQuery3Lab2.sql
+++ SQLQuery3Lab2.sql
@@ -1,4 +1,5 @@
-﻿DECLARE @Tb_Luong FLOAT 
+--Với các phòng ban có mức lương trung bình trên 30,000, liệt kê tên phòng ban và số lượng nhân viên của phòng ban đó.
+DECLARE @Tb_Luong FLOAT 
 SET @Tb_Luong = (SELECT AVG(LUONG) FROM NHANVIEN)
 SELECT TENPHG AS N'Tên phòng ban',COUNT(*) AS N'Số nhân viên'
 FROM PHONGBAN , NHANVIEN
